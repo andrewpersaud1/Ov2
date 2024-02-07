708bfcb7c829181d1fb7ab6190e46cb323f6ef2c
andrewpersaud1 <andrewpersaud022@gmail.com>
2024-02-06 19:18:20 -0500

    created seperate list

--- src/List.jsx
+++ src/List.jsx
@@ -1,27 +1,27 @@
-function List() {
-  const fruits = [
-    { id: 1, name: "apple", calories: 95 },
-    { id: 2, name: "orange", calories: 45 },
-    { id: 3, name: "banana", calories: 105 },
-    { id: 4, name: "coconut", calories: 159 },
-    { id: 5, name: "pineapple", calories: 37 },
-  ];
+function List(props) {
+  const category = props.category;
 
- // fruits.sort((a,b) =>a.name.localeCompare(b.name)); alphabetical
- // fruits.sort((a,b) => a.calories - b.calories); //Numeric
- // fruits.sort((a,b) => b.calories - a.calories);
+  const itemList = props.items;
 
-//  const lowCalFruits = fruits.filter(fruit => fruit.calories <100);
-//  const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
-
-
-  const listItems = fruits.map((fruit) => (
-                                    <li key={fruit.id}>
-                                    {fruit.name}: &nbsp;
-                                    <b>{fruit.calories}</b></li>
+  const listItems = itemList.map((item) => (
+    <li key={item.id}>
+      {item.name}: &nbsp;
+      <b>{item.calories}</b>
+    </li>
   ));
 
-  return <ol>{listItems}</ol>;
+  return (
+    <>
+      <h3 className="list-category">{category}</h3>
+      <ol className="list-items">{listItems}</ol>;
+    </>
+  );
+  // fruits.sort((a,b) =>a.name.localeCompare(b.name)); alphabetical
+  // fruits.sort((a,b) => a.calories - b.calories); //Numeric
+  // fruits.sort((a,b) => b.calories - a.calories);
+
+  //  const lowCalFruits = fruits.filter(fruit => fruit.calories <100);
+  //  const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
 }
 
 export default List;
